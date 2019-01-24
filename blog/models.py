152ce3f0cc899d168e8775d8ae7cc4126a8ff49c
psdavi <davipires.ti@gmail.com>
2019-01-24 13:45:30 -0300

    Quase finalizado

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,8 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    thumb = models.ImageField(default='default.png',blank=True)
+
 
     def publish(self):
         self.published_date = timezone.now()
@@ -17,3 +19,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def snippet(self):
+        return self.body[:50]+'...'
