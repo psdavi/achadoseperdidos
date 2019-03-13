20d81cc413259a732315bf4363c22e5a7a18e606
psdavi <davipires.ti@gmail.com>
2019-03-13 17:35:16 -0300

    first commit

--- achadoseperdidos/urls.py
+++ achadoseperdidos/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', include('blog.urls')),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
